ee6f517ec212f341a7761e22de1bdea875bc97df
gavin <czx.gavin@gmail.com>
2024-04-22 22:04:52 +0800

    compile g.729

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-emcc g729.c -O2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -Ilib -Llib -laec --pre-js preapi.js --post-js postapi.js -s EXPORTED_FUNCTIONS='["_free","_malloc"]' $@ -o g729.js
+#!/bin/bash
+emcc g729.c -O2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -Ilib -Llib -llibg729b --pre-js preapi.js --post-js postapi.js -s EXPORTED_FUNCTIONS='["_free","_malloc"]' $@ -o g729.js
--- g729.c
+++ g729.c
@@ -1,1 +1,43 @@
 #include <emscripten.h>
+
+
+#include "typedef.h"
+#include "basic_op.h"
+#include "ld8a.h"
+#include "dtx.h"
+#include "octet.h"
+
+//void Init_Pre_Process(void);
+EMSCRIPTEN_KEEPALIVE void Js_Init_Pre_Process(void){
+    Init_Pre_Process();
+}
+
+//void Init_Coder_ld8a(void)
+EMSCRIPTEN_KEEPALIVE void Js_Init_Coder_ld8a(void){
+    Init_Coder_ld8a();
+}
+
+//void Set_zero(Word16 x[],Word16 L)
+EMSCRIPTEN_KEEPALIVE void Js_Set_zero(short x[],short L){
+    Set_zero(x,L);
+}
+
+//void Init_Cod_cng(void)
+EMSCRIPTEN_KEEPALIVE void Js_Init_Cod_cng(void){
+    Init_Cod_cng();
+}
+
+//void Pre_Process(Word16 signal[],Word16 lg);
+EMSCRIPTEN_KEEPALIVE void Js_Pre_Process(short signal[],short lg){
+    Pre_Process(signal,lg);
+}
+
+//void Coder_ld8a(Word16 ana[], Word16 frame, Word16 vad_enable);
+EMSCRIPTEN_KEEPALIVE void Js_Coder_ld8a(short ana[], short frame, short vad_enable){
+    Coder_ld8a(ana, frame, vad_enable);
+}
+
+//void prm2bits_ld8k(Word16 prm[], Word16 bits[]);
+EMSCRIPTEN_KEEPALIVE void Js_prm2bits_ld8k(short prm[], short bits[]){
+    prm2bits_ld8k(prm, bits);
+}
