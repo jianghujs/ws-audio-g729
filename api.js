cb07d1be8799294f2c80cdcaef1e2bfd9272a868
wancheng1990 <25022549@qq.com>
2024-04-29 22:16:16 +0800

    update

--- api.js
+++ api.js
@@ -1,8 +1,7 @@
 import Module from './g729.js';
 
-
-class G729Encoder {
-    constructor(sampling_rate, channels, frame_duration) {
+class G729 {
+    constructor() {
         Module._Js_Init();
     }
 
@@ -22,35 +21,16 @@ class G729Encoder {
         return output;
     }
 
-    destroy() {
-    }
-}
-
-class G729Decoder {
-    constructor() {
-        this.bad_lsf = 0;
-        this.frame = 0;
-        this.vad = 0;
-        this.prm = new Int16Array(PRM_SIZE + 1);
-        this.synth = new Int16Array(L_FRAME);
-        this.Az_dec = new Int16Array(MP1 * 2);
-        this.T2 = 0;
-        this.Vad = 0;
-        Module._Js_Init_Decod_ld8a();
-        Module._Js_Init_Post_Filter();
-        Module._Js_Init_Post_Process();
-        /* for G.729b */
-        Module._Js_Init_Dec_cng();
-    }
-
     decode(data) {
-
-        // Decod_ld8a(prm, synth, Az_dec, T2, &Vad);
-        Module._Js_Decod_ld8a(this.prm, this.synth, this.Az_dec, this.T2, this.Vad);
-        // Post_Filter(synth, Az_dec, T2, Vad);        /* Post-filter */
-        Module._Js_Post_Filter(this.synth, this.Az_dec, this.T2, this.Vad);
-        // Post_Process(synth, L_FRAME);
-        Module._Js_Post_Process(this.synth, L_FRAME);
+        // input
+        const inputOffset = Module._malloc(data.length * 2);
+        Module.HEAP16.set(data, inputOffset / 2);
+        // output
+        const outputOffset = Module._malloc(data.length * 2);
+        Module._Js_Decoder(inputOffset, outputOffset);
+        // 10 -> 160
+        const output = Module.HEAP16.subarray(outputOffset / 2, outputOffset / 2 + data.length * 16);
+        return output;
     }
 
     destroy() {
@@ -59,8 +39,7 @@ class G729Decoder {
 }
 
 window.libg729 = {
-    G729Encoder,
-    G729Decoder,
+    G729,
     onload
 };
 
@@ -74,4 +53,4 @@ function onload(cb) {
     }
 }
 
-export {G729Encoder, G729Decoder, onload};
+export {G729,onload};
--- demo/demo.js
+++ demo/demo.js
@@ -1,16 +1,20 @@
-
 function loadPcm() {
     var xhr = new XMLHttpRequest();
     // 8k 16bit 1 channel
     xhr.open('GET', 'sample.pcm', true);
     xhr.responseType = 'arraybuffer';
-    xhr.onload = function() {
+    xhr.onload = function () {
         var pcm = xhr.response;
         // console.log(pcm);
         var pcmArray = new Int16Array(pcm);
-        console.log(pcmArray);
+        // console.log(pcmArray);
         // 取160个样本
         // pcmArray = pcmArray.slice(0, 160);
+
+        // 循环取160个样本
+        // for (let i = 0; i < pcmArray.length; i += 160) {
+        //     encodePcm(pcmArray.slice(i, i + 160));
+        // }
         encodePcm(pcmArray.slice(0, 160));
 
     };
@@ -18,23 +22,27 @@ function loadPcm() {
 }
 
 
-
 function encodePcm(pcmArray) {
-    window.libg729.onload(()=>{
-        var g729Encoder = new window.libg729.G729Encoder();
+    window.libg729.onload(() => {
+        var g729 = new window.libg729.G729();
         // var g729Decoder = new window.libg729.G729Decoder();
-        console.log('pcmArray', pcmArray);
-        const encodePcm =  g729Encoder.encode(pcmArray);
-        console.log('encodePcm', encodePcm);
+        console.log('需要编码的：pcmArray', pcmArray);
+        const start = new Date().getTime();
+        const encodePcm = g729.encode(pcmArray);
+        const end = new Date().getTime();
+        console.log(`时间损耗：start: ${start}, end: ${end}, time: ${end - start}`);
+        console.log('编码后的：encodePcm', encodePcm);
+        const decodePcm = g729.decode(encodePcm);
+        console.log('解码后的：decodePcm', decodePcm);
     })
 }
 
 function decodePcm() {
-    window.libg729.onload(()=>{
+    window.libg729.onload(() => {
         var g729Decoder = new window.libg729.G729Decoder();
         // var g729Decoder = new window.libg729.G729Decoder();
         console.log('pcmArray', pcmArray);
-        const encodePcm =  g729Encoder.encode(pcmArray);
+        const encodePcm = g729Encoder.encode(pcmArray);
         console.log('encodePcm', encodePcm);
     })
 }
